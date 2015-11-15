c493b16a7c80d778e838bb64bfde402fa6472c3d
Andrew-Zarenberg <andzz42@gmail.com>
2015-11-14 19:42:03 -0500

    finished registration page, added basefile

---
+++ basefile.php
@@ -0,0 +1,15 @@
+<?php include("include.php"); ?>
+
+<html>
+	<head>
+	
+		<title>PAGE TITLE HERE</title>
+		<!-- Include header -->
+		
+	</head>
+	<body>
+		<!-- Include body header -->
+		
+		<!-- Include body footer -->
+	</body>
+</html>
--- register.php
+++ register.php
@@ -1,3 +1,47 @@
+<?php include("include.php"); ?>
+
+<?php
+	// use to display errors later on
+	$errors = array();
+
+	if(isset($_POST["username"])){
+	
+		// Check if username exists
+		if($_POST["username"] == "") $errors[] = "You must enter a username.";
+		
+		if($stmt = $mysqli->prepare("SELECT username FROM member WHERE username=?")){
+			$stmt->bind_param("s", $_POST["username"]);
+			$stmt->execute();
+			$stmt->bind_result($username_exists);
+			
+			if($stmt->fetch()){
+				$errors[] = "Username already exists.";
+			}
+			$stmt->close();
+		}
+		
+		if($_POST["password1"] == "" || $_POST["password2"] == "") $errors[] = "You must enter a password.";
+		else if($_POST["password1"] != $_POST["password2"]) $errors[] = "Password do not match.";
+		
+		if($_POST["firstName"] == "") $errors[] = "You must enter a first name.";
+		if($_POST["lastName"] == "") $errors[] = "You must enter a last name.";
+		
+		$zipcode = intval($_POST["zipcode"]);
+		if($zipcode > 99999 || $zipcode < 1) $errors[] = "You must enter a valid 5-digit zipcode.";
+		
+		// If no errors, create user
+		if(empty($errors)){
+			if($stmt = $mysqli->prepare("INSERT INTO member VALUES(?, ?, ?, ?, ?)")){
+				$stmt->bind_param("ssssi", $_POST["username"], md5($_POST["password1"]), $_POST["firstName"], $_POST["lastName"], $zipcode);
+				$stmt->execute();
+				$stmt->close();
+				header("Location: index.php");
+			}
+		}
+				
+	}
+?>
+
 <html>
 	<head>
 	
@@ -7,12 +51,20 @@
 	</head>
 	<body>
 		<!-- Include body header -->
+		
+		<?php
+			if(!empty($errors)){
+				foreach($errors as $er){
+					echo "<div>" . $er . "</div>";
+				}
+			}
+		?>
 	
 		<form action="register.php" method="post">
 			<table>
 				<tr>
 					<th>Username</th>
-					<td><input name="username" /></td>
+					<td><input name="username" value="<?php if(isset($_POST["username"])) echo $_POST["username"]; ?>" /></td>
 				</tr>
 				
 				<tr>
@@ -27,17 +79,17 @@
 				
 				<tr>
 					<th>First Name</th>
-					<td><input name="firstName" /></td>
+					<td><input name="firstName" value="<?php if(isset($_POST["firstName"])) echo $_POST["firstName"]; ?>"/></td>
 				</tr>
 				
 				<tr>
 					<th>Last Name</th>
-					<td><input name="lastName" /></td>
+					<td><input name="lastName" value="<?php if(isset($_POST["lastName"])) echo $_POST["lastName"]; ?>"/></td>
 				</tr>
 				
 				<tr>
 					<th>Zipcode</th>
-					<td><input name="zipcode" /></td>
+					<td><input name="zipcode" value="<?php if(isset($_POST["zipcode"])) echo $_POST["zipcode"]; ?>"/></td>
 				</tr>
 				
 				<tr>
