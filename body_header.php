2676df5904795fa4928f6f1bf1d92764dd7db345
Andrew-Zarenberg <andzz42@gmail.com>
2015-12-02 12:17:38 -0500

    added interests page

--- body_header.php
+++ body_header.php
@@ -9,20 +9,19 @@
 	<?php
 		if(isset($_SESSION["username"])){
 			echo "Logged in as <strong>".$_SESSION["username"]."</strong>";
-			?>
-			<div style="float:right">
-				<a href="groups.php">List of Groups</a> | 
-				<a href="events.php">List of Events</a> |
-				<a href="user.php">My Profile</a> | 
-				<a href="logout.php">Log Out</a>
-			</div>
-			<?php
 		} else {
 			echo "You are not logged in";
-			echo '<div style="float:right"><a href="groups.php">List of Groups</a> | <a href="events.php">List of Events</a> | <a href="register.php">Register</a> | <a href="login.php">Log In</a></div>';
 		}
 	?>
-
+	
+	<div style="float:right">
+		<a href="groups.php">List of Groups</a> | 
+		<a href="events.php">List of Events</a> |
+		<a href="interests.php">List of Interests</a> | 
+		<a href="user.php">My Profile</a> | 
+		<a href="logout.php">Log Out</a>
+	</div>
+	
 </div>
 
 <div id="main">
